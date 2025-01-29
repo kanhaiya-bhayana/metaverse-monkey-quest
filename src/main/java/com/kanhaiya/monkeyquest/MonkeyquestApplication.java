95c5042653afe7d4220997feecab04dcf1fa5cbf
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-01-30 00:43:57 +0530

    init

---
+++ src/main/java/com/kanhaiya/monkeyquest/MonkeyquestApplication.java
@@ -0,0 +1,13 @@
+package com.kanhaiya.monkeyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanhaiya/monkeyquest/MonkeyquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanhaiya.monkeyquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
