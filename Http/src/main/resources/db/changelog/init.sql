d10e68e24fd0fdb5ffc81f203fda9b88f6143e6c
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-02-02 15:59:19 +0530

    Liquibase: script fixed

--- Http/src/main/resources/db/changelog/init.sql
+++ Http/src/main/resources/db/changelog/init.sql
@@ -1,9 +1,8 @@
 -- liquibase formatted sql
 
--- changeset kanhaiya:1
-DROP TYPE IF EXISTS your_enum_name;
-CREATE TYPE your_enum_name AS ENUM (
-    'value1',
-    'value2',
-    'value3'
+-- changeset kanhaiya:3
+DROP TYPE IF EXISTS user_role;
+CREATE TYPE user_role AS ENUM (
+    'ADMIN',
+    'USER'
 );
--- Http/src/main/resources/db/changelog/terimaki.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset kanhaiya:2
-DROP TYPE IF EXISTS user_role;
-CREATE TYPE user_role AS ENUM (
-    'ADMIN',
-    'USER',
-    'BAAP'
-);
